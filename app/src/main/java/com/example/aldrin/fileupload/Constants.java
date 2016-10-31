e9aa952f89d28b9e0915ba34d2345bd539dff0e8
aldrin <aldrin@qbusrt.com>
2016-10-31 18:39:35 +0530

    file uplaod tes app created

---
+++ app/src/main/java/com/example/aldrin/fileupload/Constants.java
@@ -0,0 +1,11 @@
+package com.example.aldrin.fileupload;
+
+/**
+ * Created by aldrin on 31/10/16.
+ */
+
+public class Constants {
+
+    public static final int REQUEST_CAMERA = 1;
+    public static final int UPLOAD_IMAGE = 2;
+}
--- app/src/main/java/com/example/aldrin/fileupload/MainActivity.java
+++ app/src/main/java/com/example/aldrin/fileupload/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.aldrin.fileupload;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Constants.UPLOAD_IMAGE)  {
+
+            } else if (requestCode == Constants.REQUEST_CAMERA){
+
+            }
+        }
+    }
+
+    @OnClick(R.id.btn_start_camera)
+    void takePhoto() {
+        intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, Constants.REQUEST_CAMERA);
+    }
+
+    @OnClick(R.id.btn_upload_image)
+    void uploadPhoto() {
+        intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select image"), Constants.UPLOAD_IMAGE);
     }
 }
